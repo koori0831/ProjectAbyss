e9073d1da9faa3bff8d1f88d91bcef4425fe8ca7
koori0831 <fuyunokoori0831@gmail.com>
2024-10-30 19:25:20 +0900

    ADD : Odin

---
+++ Assets/GGMPool/RunTime/PoolTypeSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGMPool
+{
+    [CreateAssetMenu(menuName = "SO/Pool/Type")]
+    public class PoolTypeSO : ScriptableObject
+    {
+        public string typeName;
+    }
+}
