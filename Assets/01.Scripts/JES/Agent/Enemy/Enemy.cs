31211a9a2359ae40b54fd329c18a47e635d96404
jes0321 <jesjes0321@naver.com>
2024-11-20 18:58:34 +0900

    ADD:LayerSetting

--- Assets/01.Scripts/JES/Agent/Enemy/Enemy.cs
+++ Assets/01.Scripts/JES/Agent/Enemy/Enemy.cs
@@ -1,4 +1,39 @@
+using System;
+using UnityEngine;
+
 public class Enemy : Entity
 {
+    protected StateMachine<EnemyStateType> stateMachine;
+
+    [Header("Direct")] 
+    public float ditectRange;
+    [SerializeField] private LayerMask _whatIsTaget;
+
+    [Header("Combat")] 
+    public int damage;
+    public float knockPower;
+    public float attackRange;
+    public Player Target { get; private set; }
+
+    public bool DitectTarget()
+    {
+        return true;
+    }
     
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, ditectRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position,attackRange);
+        Gizmos.color = Color.white;
+    }
+    #endif
+}
+
+
+public enum EnemyStateType
+{
+    Idle,Run
 }
