c373e15dd15a9f27e0d11a97f9fadddb5166951a
jes0321 <jesjes0321@naver.com>
2024-11-27 19:26:33 +0900

    fix : entity compo

--- Assets/01.Scripts/JES/Agent/Enemy/RangeGroundEnemy/State/RangeGroundEnemyWaitState.cs
+++ Assets/01.Scripts/JES/Agent/Enemy/RangeGroundEnemy/State/RangeGroundEnemyWaitState.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 public class RangeGroundEnemyWaitState : State<RangeGroundEnemyStateType>
 {
     private RangeGroundEnemy _enemy  = null;
+    private RangeEnemyAtkCompo _atkCompo = null;
     public RangeGroundEnemyWaitState(Entity entity, string animaName, StateMachine<RangeGroundEnemyStateType> stateMachine) : base(entity, animaName, stateMachine)
     {
         _enemy = entity as RangeGroundEnemy;
+        _atkCompo = _enemy.GetCompo<RangeEnemyAtkCompo>();
     }
 
     public override void StateUpdate()
     {
         base.StateUpdate();
-        if (!(_enemy.lastAttackTime + _enemy.attackCooldown <= Time.time)) return;
+        if (!_atkCompo.CanAttack()) return;
         
         float distance =Vector3.Distance(_entity.transform.position, _enemy.Target.transform.position);
 
