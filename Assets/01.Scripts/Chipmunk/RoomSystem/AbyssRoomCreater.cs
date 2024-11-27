5bff40344133fed4595b5b97d413ba5d206f6e3a
Chipmunk-Rex <rexgood1005@gmail.com>
2024-11-27 19:11:36 +0900

    feat : map Creater

--- Assets/01.Scripts/Chipmunk/RoomSystem/AbyssRoomCreater.cs
+++ Assets/01.Scripts/Chipmunk/RoomSystem/AbyssRoomCreater.cs
@@ -6,8 +6,38 @@ public class AbyssRoomCreater : MonoBehaviour
 {
     [field: SerializeField] public Tilemap AbyssTilemap { get; private set; }
     [field: SerializeField] List<AbyssRoomSO> abyssRooms { get; set; }
-    public void CreateRoom()
+    [SerializeField] int roomsBetweenSpace = 30;
+    [SerializeField] int lastGeneratedY = 0;
+    public void CreateRoom(int yPos, int holeWidth)
     {
+        if (yPos >= lastGeneratedY - roomsBetweenSpace)
+            return;
 
+        AbyssRoomSO room = abyssRooms[Random.Range(0, abyssRooms.Count - 1)];
+        bool isLeft = Random.Range(0, 2) == 0;
+
+        for (int x = 0; x < room.mapSize.x; x++)
+        {
+            for (int y = room.mapSize.y; y > 0; y--)
+            {
+                {
+                    int xPos = x + holeWidth / 2;
+
+                    AbyssTilemap.SetTile(new Vector3Int(isLeft ? -xPos : xPos, y + yPos, 0), null);;
+                }
+                Vector2Int pos = new Vector2Int(x, room.mapSize.y - y);
+                if (room.blockDatas.TryGetValue(pos, out AbyssRoomBlockData blockData))
+                {
+                    int xPos = x + holeWidth / 2;
+                    if (isLeft)
+                    {
+                        xPos = -xPos;
+                    }
+                    Vector2Int worldPos = new Vector2Int(xPos, y + yPos);
+                    blockData.OnTiled(this, worldPos);
+                }
+            }
+        }
+        lastGeneratedY = yPos;
     }
 }
