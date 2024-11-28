dbe069ad7daebf07ed54da996659669da43c49a2
jes0321 <jesjes0321@naver.com>
2024-11-28 20:07:19 +0900

    fix : JESScene

--- Assets/01.Scripts/JES/Agent/Enemy/Enemy.cs
+++ Assets/01.Scripts/JES/Agent/Enemy/Enemy.cs
@@ -8,11 +8,7 @@ public abstract class Enemy : Entity
     [SerializeField] protected LayerMask _whatIsTarget;
     
     [Header("Combat")] 
-    public int damage;
-    public float knockPower;
     public float attackRange;
-    public float lastAttackTime=0;
-    public float attackCooldown=1.0f;
     public Player Target { get; protected set; }
 
     /// <summary>
