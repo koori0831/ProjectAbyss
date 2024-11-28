839d5f6a6bee3ad05c3a10854a253f88abc82acc
Chipmunk-Rex <rexgood1005@gmail.com>
2024-11-28 19:46:42 +0900

    Revert "Merge branch 'Chipmunk' into base"
    This reverts commit ec00d4a39cd4505ae6a903f4c04ebb3c627a1b81, reversing
    changes made to 2a91c67228f26bba57a5c4613d10472dbe01f3df.

--- Assets/01.Scripts/Chipmunk/RoomSystem/AbyssRoomCreater.cs
+++ Assets/01.Scripts/Chipmunk/RoomSystem/AbyssRoomCreater.cs
@@ -6,53 +6,8 @@ public class AbyssRoomCreater : MonoBehaviour
 {
     [field: SerializeField] public Tilemap AbyssTilemap { get; private set; }
     [field: SerializeField] List<AbyssRoomSO> abyssRooms { get; set; }
-    [SerializeField] int roomsBetweenSpace = 30;
-    [SerializeField] int lastGeneratedY = 0;
-    public void CreateRoom(int yPos, int holeWidth)
+    public void CreateRoom()
     {
-        if (yPos >= lastGeneratedY - roomsBetweenSpace)
-            return;
 
-        AbyssRoomSO room = abyssRooms[Random.Range(0, abyssRooms.Count)];
-        bool isLeft = Random.Range(0, 2) == 0;
-
-        for (int x = 0; x < room.mapSize.x; x++)
-        {
-            for (int y = room.mapSize.y; y > 0; y--)
-            {
-                {
-                    int xPos = x + holeWidth / 2;
-
-                    AbyssTilemap.SetTile(new Vector3Int(isLeft ? -xPos : xPos, y + yPos, 0), null); ;
-                }
-                Vector2Int pos = new Vector2Int(x, room.mapSize.y - y);
-                if (room.blockDatas.TryGetValue(pos, out AbyssRoomBlockData blockData))
-                {
-                    int xPos = x + holeWidth / 2;
-                    if (isLeft)
-                    {
-                        xPos = -xPos;
-                    }
-                    Vector2Int worldPos = new Vector2Int(xPos, y + yPos);
-                    blockData.OnTiled(this, worldPos);
-                }
-            }
-        }
-        lastGeneratedY = yPos;
     }
-
-#if UNITY_EDITOR
-    [ContextMenu("AutoSetRooms")]
-    public void AutoSetRooms()
-    {
-        abyssRooms = new List<AbyssRoomSO>();
-        string[] guids = UnityEditor.AssetDatabase.FindAssets("t:AbyssRoomSO");
-        foreach (var guid in guids)
-        {
-            string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
-            AbyssRoomSO room = UnityEditor.AssetDatabase.LoadAssetAtPath<AbyssRoomSO>(path);
-            abyssRooms.Add(room);
-        }
-    }
-#endif
 }
