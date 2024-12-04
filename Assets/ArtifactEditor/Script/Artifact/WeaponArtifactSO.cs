d350267e739f6ebd114eecfafbefd9e22c8218c8
koori0831 <fuyunokoori0831@gmail.com>
2024-12-04 22:41:05 +0900

    feat : WeaponArtifactSO

--- Assets/ArtifactEditor/Script/Artifact/WeaponArtifactSO.cs
+++ Assets/ArtifactEditor/Script/Artifact/WeaponArtifactSO.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 public class WeaponArtifactSO : ArtifactSO
 {
     [Header ("Weapon Info")]
-    int weaponAtk = 5;
+    [field: SerializeField] public WeaponType WeaponType = WeaponType.Melee;
+    [field: SerializeField] public int WeaponAtk = 5;
+    [field: SerializeField] public float AtkRange = 5;
+    [field: SerializeField] public float AtkSpeed = 5;
+    [field: SerializeField] public int KnockBackPower = 5;
+    [field: SerializeField] public SkillSO SkillSO { get; private set; }
 }
